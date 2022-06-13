64b779d3a9a3eac1b982aa11847b116c87eebff6
Giuseppe-lore <loregiuseppe0@gmail.com>
2022-06-13 15:38:36 +0200

    Palindrome complete

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -1,11 +1,15 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 
-// // Chiedo all’utente di inserire una parola attraverso un prompt:
-const userWord = prompt('Dimmi una parola');
-const userWordPalindromeOrNot = isPalindrome(userWord)
 
-if(userWordPalindromeOrNot === 'yes') {
+// Chiedo all’utente di inserire una parola attraverso un prompt:
+const userWord = prompt('Dimmi una parola');
+
+// Invoco la mia funzione:
+const userWordPalindromeOrNot = isPalindrome(userWord);
+
+// Creo un if per il messaggio di output per l'utente: 
+if(userWordPalindromeOrNot === true) {
     alert('la parola è palindroma')
 } else {
     alert('la parola non è palindroma')
@@ -13,13 +17,15 @@ if(userWordPalindromeOrNot === 'yes') {
 
 // funzione che verifica se la parola è palindroma o no;
 // word --> parola da valutare
-// return --> stringa: 'yes' se palindroma, altrimenti 'no'.
+// return --> stringa: 'true' se palindroma, altrimenti 'false'.
 function isPalindrome(word) {
 
     // Dichiaro una variabile che conterrà di volta in volta 
     // le lettere che il ciclo for successivamente scorrerà al contrario :
-    let reverseWord;
-    let palindromeOrNot = 'no';
+    let reverseWord = '';
+
+    // dichiaro di default che la parola che inserirà l'utente non sarà palindroma, attraverso il valore = false della variabile:
+    let palindromeOrNot = false;
    
     // creo un ciclo for che scorrerà al contrario le lettere della parola inserita dall'utente:
     for(let i = userWord.length - 1; i >= 0; i--) {
@@ -27,16 +33,16 @@ function isPalindrome(word) {
         // aggiungo le lettere nella variabile:
         reverseWord += userWord[i];  
     }
-
+    
     // Se la parola dell'utente è uguale alla stessa al contrario, il messaggio di output sarà "yes"
     if(userWord === reverseWord) {
-        palindromeOrNot = 'yes'     
+        palindromeOrNot = true     
     }
-
+    
     // ritorno il messaggio di output:
-    return palindromeOrNot
+    return palindromeOrNot;
 }
- 
+
 
 
 
